94d5b1041d95b7af8c8ab74adb02ac1820a99466
Hermano Flávio de Moura <hermanoudi@gmail.com>
2025-06-01 18:18:31 -0300

    feat: :bricks: insert sqlalchemy and migrations

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,137 @@
-from fastapi import FastAPI
+from http import HTTPStatus
 
-app = FastAPI()
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
+
+from fastapi_zero.database import get_session
+from fastapi_zero.models import User
+from fastapi_zero.schemas import (
+    Message,
+    UserList,
+    UserPublic,
+    UserSchema,
+)
+
+app = FastAPI(title='Fastapi Zero Hermano')
+
+database = []
 
 
-@app.get('/')
+@app.get(
+    '/',
+    status_code=HTTPStatus.OK,
+    response_model=Message,
+)
 def read_root():
     return {'message': 'olá Mundo!'}
+
+
+def test_health_check(client):
+    response = client.get('/health')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'ok'}
+
+
+@app.get(
+    '/health',
+    status_code=HTTPStatus.OK,
+    response_model=Message,
+)
+def health_check():
+    return {'message': 'ok'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+    db_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
+        )
+    )
+
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT,
+                detail='Username already exists',
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT,
+                detail='Email already exists',
+            )
+
+    db_user = User(
+        username=user.username, password=user.password, email=user.email
+    )
+
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
+
+
+@app.get('/users/', response_model=UserList)
+def read_users(
+    skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
+):
+    users = session.scalars(select(User).offset(skip).limit(limit)).all()
+    return {'users': users}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(
+    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+):
+    user_db = session.scalar(select(User).where(User.id == user_id))
+
+    if not user_db:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    try:
+        user_db.email = user.email
+        user_db.username = user.username
+        user_db.password = user.password
+        session.add(user_db)
+        session.commit()
+        session.refresh(user_db)
+    except IntegrityError:
+        raise HTTPException(
+            status_code=HTTPStatus.CONFLICT,
+            detail='Username or Email already exists',
+        )
+
+    return user_db
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int, session: Session = Depends(get_session)):
+    user_db = session.scalar(select(User).where(User.id == user_id))
+
+    if not user_db:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    session.delete(user_db)
+    session.commit()
+
+    return {'message': 'User deleted'}
+
+
+@app.get('/users/{user_id}', response_model=UserPublic)
+def read_user__exercicio(
+    user_id: int, session: Session = Depends(get_session)
+):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+
+    if not db_user:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+
+    return db_user
